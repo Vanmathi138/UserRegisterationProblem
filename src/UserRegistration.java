96b7bc72f8dde6059d4b31cb63eecd8151d81ad9
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2025-05-19 17:34:10 +0530

    valid email

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -6,6 +6,7 @@ public class UserRegistration {
     //attributes
     private String firstName;
     private String lastName;
+    private String email;
 
     // setter
     public void setFirstName (String firstName){
@@ -14,17 +15,27 @@ public class UserRegistration {
     public void setLastName(String lastName){
         this.lastName = lastName;
     }
+    public void setEmail(String email){
+        this.email = email;
+    }
 
     //getter
     public String getFirstName(){
         return firstName;
     }
-    private String getLastName() {
+    public String getLastName() {
         return lastName;
     }
+    public String getEmail(){
+        return email;
+    }
+
 
     public static boolean isValidName(String firstName){
         return firstName.matches("^[A-Z][a-zA-Z]{2,}$");
+    }
+    public static boolean isValidEmail(String email){
+        return email.matches("^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@bl\\.co(\\.[a-zA-Z]{2,}){1,2}$");
     }
 
     public static void main(String[] args) {
@@ -36,17 +47,24 @@ public class UserRegistration {
         String first_name = scanner.next();
         System.out.print("Enter last name: ");
         String last_name = scanner.next();
+        System.out.print("Enter Email id: ");
+        String email_id = scanner.next();
+
 
         if(isValidName(first_name) && isValidName(last_name)){
             userRegistration.setFirstName(first_name);
             userRegistration.setLastName(last_name);
-            System.out.println("\nValid first name: "+
-                    userRegistration.getFirstName()+
-                    "\nValid last name: "+
-                    userRegistration.getLastName());
+            System.out.println("\nValid first name: "+ userRegistration.getFirstName()+
+                    "\nValid last name: "+ userRegistration.getLastName());
         }else {
             System.out.println("First name & Last name should start with a capital letter and be at least 3 characters long.");
 
+        }
+        if(isValidEmail(email_id)){
+            userRegistration.setEmail(email_id);
+            System.out.println("Valid email: "+ userRegistration.getEmail());
+        }else {
+            System.out.println("Invalid email format. Example: abc.xyz@bl.co.in");
         }
     }
 
